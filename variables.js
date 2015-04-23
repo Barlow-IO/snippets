6280ec3dbe50f23e9d7c041497be6edb15741373
Matt T <mstomasze@aol.com>
2015-04-23 14:59:40 -0400

    Basic Start of Statements
    keep it real yo

---
+++ variables.js
@@ -0,0 +1,24 @@
+//Basic defining and math
+var a = 1;
+a = a + 2;
+console.log(a);
+
+//You can declare decimals, strings with " and '
+var pi = 3.14;
+var nameDude = "Billy Ray Cyrus";
+var nameChick = 'Miley Cyrus';
+
+//You can create a string, then declare it later
+var snippetName;
+snippetName = "This is counter-productive";
+
+//You can declare multiple strings at once
+var dudeMan = "Bill", ladyWoman = "Jill", petDog = "Fido";
+
+//Declaring multiple strings with a different look
+var duderrino = "Kevin",
+    ladyerrino = "Seven",
+    peterrino = "Flevin";
+
+//Concatenation (putting together strings)
+var b = "Billy" + " " + "Mays";
